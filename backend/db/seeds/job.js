817571b18c0f1dade1a68a488e67ba9741beac4d
zakariasaad <zakaria.elas@gmail.com>
2020-08-19 02:04:12 +0100

    fix: seed script by mapping questions to attributes

--- backend/db/seeds/job.js
+++ backend/db/seeds/job.js
@@ -1,93 +1,6 @@
 const db = require('../index');
 
 const createJob = async (job_id) => {
-  let questions = [
-    {
-      text: 'What is the Virtual DOM?',
-      attributes: [
-        {
-          attribute_id: '5f2c158446289852da7ed12a',
-          attribute_name: 'Platform Internals',
-        },
-        {
-          attribute_id: '5f2c158446289852da7ed122',
-          attribute_name: 'Tool Knowledge',
-        },
-      ],
-    },
-    {
-      text: 'Why do we use JavaScript frameworks?',
-      attributes: [
-        {
-          attribute_id: '5f2c158446289852da7ed12a',
-          attribute_name: 'Platform Internals',
-        },
-        {
-          attribute_id: '5f2c158446289852da7ed122',
-          attribute_name: 'Tool Knowledge',
-        },
-      ],
-    },
-    {
-      text: 'What is the difference between async await and promises?',
-      attributes: [
-        {
-          attribute_id: '5f2c158446289852da7ed12a',
-          attribute_name: 'Platform Internals',
-        },
-        {
-          attribute_id: '5f2c158446289852da7ed122',
-          attribute_name: 'Tool Knowledge',
-        },
-      ],
-    },
-    {
-      text: 'What does an async function return in JavaScript?',
-      attributes: [
-        {
-          attribute_id: '5f2c158446289852da7ed12a',
-          attribute_name: 'Platform Internals',
-        },
-        {
-          attribute_id: '5f2c158446289852da7ed122',
-          attribute_name: 'Tool Knowledge',
-        },
-        {
-          attribute_id: '5f2c158446289852da7ed12b',
-          attribute_name: 'Languages Exposed to',
-          weight: 0.5,
-        },
-      ],
-    },
-    {
-      text: 'What is the purpose of CI/CD?',
-      attributes: [
-        {
-          attribute_id: '5f2c158446289852da7ed132',
-          attribute_name: 'Build Automation',
-        },
-        {
-          attribute_id: '5f2c158446289852da7ed135',
-          attribute_name: 'Automated Testing',
-        },
-      ],
-    },
-    'Can you tell me about one of your side projects?',
-    'When would you choose to use Typescript over JavaScript?',
-    {
-      text: 'What would you change in Javascript?',
-      attributes: [
-        {
-          attribute_id: '5f2c158446289852da7ed12b',
-          attribute_name: 'Languages Exposed to',
-          weight: 0.25,
-        },
-      ],
-    },
-    'Can you tell me a bit about the event loop?',
-    'What do you know about microtask queues in JavaScript?',
-  ];
-
   let attributes = [
     {
       name: 'Version Control',
@@ -229,13 +142,115 @@ const createJob = async (job_id) => {
     },
   ];
 
-  questions = questions.map((q) => (q.text ? q : { text: q }));
-
-  const job = await db.Job.create({
+  let job = await db.Job.create({
     job_id,
-    questions,
+    questions: [],
     scorecard: attributes,
   });
+
+  const scorecardByName = job
+    .toObject()
+    .scorecard.reduce(
+      (acc, curr) => ({ ...acc, [curr.name]: { ...curr } }),
+      {},
+    );
+
+  console.log(scorecardByName);
+
+  let questions = [
+    {
+      text: 'What is the Virtual DOM?',
+      attributes: [
+        {
+          attribute_id: scorecardByName['Platform Internals']._id,
+          attribute_name: 'Platform Internals',
+        },
+        {
+          attribute_id: scorecardByName['Tool Knowledge']._id,
+          attribute_name: 'Tool Knowledge',
+        },
+      ],
+    },
+    {
+      text: 'Why do we use JavaScript frameworks?',
+      attributes: [
+        {
+          attribute_id: scorecardByName['Platform Internals']._id,
+          attribute_name: 'Platform Internals',
+        },
+        {
+          attribute_id: scorecardByName['Tool Knowledge']._id,
+          attribute_name: 'Tool Knowledge',
+        },
+      ],
+    },
+    {
+      text: 'What is the difference between async await and promises?',
+      attributes: [
+        {
+          attribute_id: scorecardByName['Platform Internals']._id,
+          attribute_name: 'Platform Internals',
+        },
+        {
+          attribute_id: scorecardByName['Tool Knowledge']._id,
+          attribute_name: 'Tool Knowledge',
+        },
+      ],
+    },
+    {
+      text: 'What does an async function return in JavaScript?',
+      attributes: [
+        {
+          attribute_id: scorecardByName['Platform Internals']._id,
+          attribute_name: 'Platform Internals',
+        },
+        {
+          attribute_id: scorecardByName['Tool Knowledge']._id,
+          attribute_name: 'Tool Knowledge',
+        },
+        {
+          attribute_id: scorecardByName['Languages Exposed to']._id,
+          attribute_name: 'Languages Exposed to',
+          weight: 0.5,
+        },
+      ],
+    },
+    {
+      text: 'What is the purpose of CI/CD?',
+      attributes: [
+        {
+          attribute_id: scorecardByName['Build Automation']._id,
+          attribute_name: 'Build Automation',
+        },
+        {
+          attribute_id: scorecardByName['Automated Testing']._id,
+          attribute_name: 'Automated Testing',
+        },
+      ],
+    },
+    'Can you tell me about one of your side projects?',
+    'When would you choose to use Typescript over JavaScript?',
+    {
+      text: 'What would you change in Javascript?',
+      attributes: [
+        {
+          attribute_id: scorecardByName['Languages Exposed to']._id,
+          attribute_name: 'Languages Exposed to',
+          weight: 0.25,
+        },
+      ],
+    },
+    'Can you tell me a bit about the event loop?',
+    'What do you know about microtask queues in JavaScript?',
+  ];
+
+  questions = questions.map((q) => (q.text ? q : { text: q }));
+
+  job = await db.Job.findOneAndUpdate(
+    { job_id: job.job_id },
+    { $set: { questions } },
+    { new: true },
+  );
   return job;
 };
 
